89d2487e85b198fd7b8f75aae7b300ef0b7588eb
JonatV <47326572+JonatV@users.noreply.github.com>
2025-03-05 22:37:02 +0100

    feat: add factory utils

---
+++ Factory_utils.cpp
@@ -0,0 +1,43 @@
+#include "Factory.hpp"
+#include "colors.hpp"
+#include <iostream>
+#include <map>
+
+void	Factory::findSetProperty(const std::string &property, const std::string &answer)
+{
+	static const std::map<std::string, void (Factory::*)(const std::string &)> setters = {
+		{"Name", &Factory::setName},
+		{"Type", &Factory::setType},
+		{"Location", &Factory::setLocation},
+		{"Floor", &Factory::setFloor},
+		{"Size", &Factory::setSize}
+	};
+	static const std::map<std::string, std::string (Factory::*)() const> getters = {
+		{"Name", &Factory::getName},
+		{"Type", &Factory::getType},
+		{"Location", &Factory::getLocation},
+		{"Floor", &Factory::getFloor},
+		{"Size", &Factory::getWidth}
+	};
+
+	auto posSetter = setters.find(property);
+	auto posGetter = getters.find(property);
+	if (posSetter != setters.end())
+	{
+		if (VERBOSE)
+			std::cout << GREY "Property found: " << posSetter->first << RESET << std::endl;
+		std::string valueCheck = (this->*(posGetter->second))();
+		if (!valueCheck.empty())
+		{
+			std::cout << "Property already set: " << valueCheck << std::endl; // wip can throw an error if multiple similar properties
+			return;
+		}
+		(this->*(posSetter->second))(answer);
+	}
+	else
+	{
+		std::cout << "Property not found" << std::endl;
+	}
+	// map on each getter to check if one of the property is still empty
+	// if not, dont change Factory::_allButLayoutFound
+}
