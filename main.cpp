898719c29f71955540044298de7ce9d1cff293bb
JonatV <47326572+JonatV@users.noreply.github.com>
2025-03-06 00:36:06 +0100

    fix: update the main the avoid  the parse object

--- main.cpp
+++ main.cpp
@@ -1,9 +1,49 @@
-#include "Parser.hpp"
+#include "Factory.hpp"
 #include "colors.hpp"
 #include <iostream>
 
+static bool	checkProgramArgs(int ac)
+{
+	try
+	{
+		if (ac == 1)
+			throw std::runtime_error("You must include a file");
+		if (ac > 2)
+			throw std::runtime_error("Program must have only one file or argument");
+	}
+	catch (const std::exception &e) 
+	{
+		std::cout << RED "Error: " BWHITE << e.what() << RESET << std::endl;
+		return (false);
+	}
+	catch (...)
+	{
+		std::cout << RED "Error: Unexpected exception." BWHITE << RESET << std::endl;
+		return (false);
+	}
+	return (true);
+}
+
 int	main(int ac, char **av)
 {
-	runParser(ac, av);
+	// verify arguments
+	if (!checkProgramArgs(ac))
+		return (1);
+	if (VERBOSE)
+		std::cout << GREEN "Program arguments verified and valid." RESET << std::endl;
+	Factory	*factory = new Factory(av[1]);
+	try
+	{
+		factory->parseFactory();
+	}
+	catch (const std::exception &e)
+	{
+		std::cout << RED "Error: " BWHITE << e.what() << RESET << std::endl;
+	}
+	catch (...)
+	{
+		std::cout << RED "Error: Unexpected exception." BWHITE << RESET << std::endl;
+	}
+	delete factory;
 	return (0);
 }
